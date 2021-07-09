e4e621a604bbd5c9e786ddf1d4a84d0eceb3ad61
takashimamorino <does1026@gmail.com>
2021-07-10 06:03:25 +0900

    chore

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from "@nestjs/common";
+import { User } from "src/domain/entities/user.entity";
+import { UsersService } from 'src/domain/services/user.service';
 
 @Controller('/users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
   @Get()
-  findAll(): string {
-    console.log('hoge')
-    return 'hoge'
+  findAll(): User[] {
+    return this.usersService.findAll()
   }
 }
