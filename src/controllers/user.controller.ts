3159f2e2c6d8fb7126149c8e7d82e14c3e5fd2ca
takashimamorino <does1026@gmail.com>
2021-07-10 14:31:05 +0900

    chore

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,12 +2,17 @@ import { Controller, Get } from "@nestjs/common";
 import { User } from "src/domain/entities/user.entity";
 import { UsersService } from 'src/domain/services/user.service';
 
-@Controller('/users')
+@Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
-  @Get()
-  findAll(): User[] {
+  @Get('/users')
+  async findAll(): Promise<User[]> {
     return this.usersService.findAll()
+  }
+
+  @Get('/user')
+  async find(): Promise<User> {
+    return this.usersService.find()
   }
 }
--- src/domain/services/user.service.ts
+++ src/domain/services/user.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/domain/entities/user.entity';
 
+const dummuy: User[] = [
+  {
+    id: '01',
+    name: 'name01',
+    age: 20
+  },
+  {
+    id: '02',
+    name: 'name02',
+    age: 20
+  },
+  {
+    id: '03',
+    name: 'name03',
+    age: 20
+  }
+]
+
 @Injectable()
 export class UsersService {
-  private readonly users: User[] = [];
+  private readonly users: User[] = dummuy;
 
-  findAll(): User[] {
+  async findAll(): Promise<User[]> {
     return this.users;
+  }
+
+  async find(): Promise<User> {
+    return this.users[0];
   }
 }
